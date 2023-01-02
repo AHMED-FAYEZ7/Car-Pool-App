90c11b116ea52b1c3aa68191c80a0665ff8bedb9
Mahmoud <mahmoudmohamed74>
2023-01-02 18:34:29 +0200

    location

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kau_carpool/layout/app_layout.dart';
+import 'package:kau_carpool/pages/confirm/confirm_page.dart';
 import 'package:kau_carpool/pages/login/login_page.dart';
 import 'package:kau_carpool/pages/register/register_page.dart';
 import 'package:kau_carpool/pages/requests/requests_page.dart';
 import 'bloc_observer.dart';
 import 'firebase_options.dart';
 
-void main() async{
-  BlocOverrides.runZoned(() async
-  {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-  },
-      blocObserver: MyBlocObserver(),
+void main() async {
+  BlocOverrides.runZoned(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      runApp(const MyApp());
+    },
+    blocObserver: MyBlocObserver(),
   );
 }
 
@@ -29,13 +30,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        LoginPage.id : (context) => LoginPage(),
-        RegisterPage.id : (context) => RegisterPage(),
-        AppLayout.id : (context) => AppLayout(),
-        RequestsPage.id : (context) => RequestsPage(),
+        LoginPage.id: (context) => LoginPage(),
+        RegisterPage.id: (context) => RegisterPage(),
+        AppLayout.id: (context) => AppLayout(),
+        RequestsPage.id: (context) => RequestsPage(),
+        ConfirmPage.id: (context) => ConfirmPage(),
       },
-      initialRoute: AppLayout.id,
+      initialRoute: ConfirmPage.id,
     );
   }
 }
-
---
+++ lib/pages/confirm/confirm_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:kau_carpool/helper/resources/color_manager.dart';
+import 'package:kau_carpool/widgets/default_appbar.dart';
+
+class ConfirmPage extends StatelessWidget {
+  ConfirmPage({Key? key}) : super(key: key);
+  static String id = 'ConfirmPage';
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: DefaultAppBar(
+        title: "Rider Requests list",
+      ),
+      backgroundColor: ColorManager.backgroundColor,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 80 / 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: ColorManager.white,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset.zero,
+                    blurRadius: 20,
+                    color: ColorManager.white,
+                  ),
+                ],
+              ),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Center(
+                      child: Container(
+                        width: 350,
+                        height: 50,
+                        child: TextField(
+                          onChanged: (value) {
+                            //  searchData(st = value.trim().toLowerCase());
+                            // Method For Searching
+                          },
+                          textAlign: TextAlign.justify,
+                          decoration: InputDecoration(
+                            hintText: "Search for a rider",
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          "Requests",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: ColorManager.blueWithOpacity,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
