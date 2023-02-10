3238503aecfced162c516adee8a371d4bcfaadaa
faz <ahmedfayez834@gmail.com>
2023-02-10 19:22:58 +0200

    finish commit

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -317,8 +317,8 @@ class _HomePageState extends State<HomePage> {
                                                         .text,
                                                 pickUpLocation:
                                                     findPickUpController.text,
-                                                dropOffLocation: "alex",
-                                                    // findDropOffController.text,
+                                                dropOffLocation:
+                                                    findDropOffController.text,
                                               );
                                               Navigator.push(
                                                 context,
@@ -385,8 +385,8 @@ class _HomePageState extends State<HomePage> {
                                               pickUpLocation:
 
                                                       findPickUpController.text,
-                                              dropOffLocation: "alex",
-                                                  // findDropOffController.text,
+                                              dropOffLocation:
+                                                  findDropOffController.text,
                                               dateTime:
                                                   offerDateAndTimeController
                                                       .text,
